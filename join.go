b04d326baf62c503ee6dce2a56ded480b087a1a1
Aleksa Sarai <cyphar@cyphar.com>
2024-05-03 21:58:59 +1000

    *: update copyright lines
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- join.go
+++ join.go
@@ -1,5 +1,5 @@
 // Copyright (C) 2014-2015 Docker Inc & Go Authors. All rights reserved.
-// Copyright (C) 2017 SUSE LLC. All rights reserved.
+// Copyright (C) 2017-2024 SUSE LLC. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 

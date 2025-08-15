8029c4c0315a967561574ee5c77e9adbb67d895e
Aleksa Sarai <cyphar@cyphar.com>
2025-08-15 14:02:44 +1000

    gocompat/generics: add Go authors copyright line
    Most of this code is copied (or heavily based on) the Go stdlib code, so
    we need to include their copyright headers too.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- gocompat_generics_unsupported.go
+++ gocompat_generics_unsupported.go
@@ -2,6 +2,7 @@
 
 //go:build linux && !go1.21
 
+// Copyright (C) 2021, 2022 The Go Authors. All rights reserved.
 // Copyright (C) 2024 SUSE LLC. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.

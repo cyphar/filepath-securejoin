069d5ffe0638668d8d524e41acbbc5db6f888edc
Aleksa Sarai <cyphar@cyphar.com>
2025-08-29 17:53:26 +1000

    internal: add kernelversion package
    This was originally intended to be a simple copy of runc's internal
    kernelversion package (which was derived from containerd, in turn
    derived from Docker). However, the runc one is quite over-complicated
    and uses quite confusing terminology and so I went ahead and wrote a
    more generic but less complicated version.
    
    It is very loosely based on the Go stdlib's kernelversion but completely
    rewritten and extended to support more than 2-digit kernel versions (for
    2.6.x support) and with quite a few added tests.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- internal/gocompat/gocompat_generics_go121.go
+++ internal/gocompat/gocompat_generics_go121.go
@@ -9,6 +9,7 @@
 package gocompat
 
 import (
+	"cmp"
 	"slices"
 	"sync"
 )
@@ -36,4 +37,17 @@ func SyncOnceValue[T any](f func() T) func() T {
 // SyncOnceValues is equivalent to Go 1.21's sync.OnceValues.
 func SyncOnceValues[T1, T2 any](f func() (T1, T2)) func() (T1, T2) {
 	return sync.OnceValues(f)
+}
+
+// CmpOrdered is equivalent to Go 1.21's cmp.Ordered generic type definition.
+type CmpOrdered = cmp.Ordered
+
+// CmpCompare is equivalent to Go 1.21's cmp.Compare.
+func CmpCompare[T CmpOrdered](x, y T) int {
+	return cmp.Compare(x, y)
+}
+
+// Max2 is equivalent to Go 1.21's max builtin (but only for two parameters).
+func Max2[T CmpOrdered](x, y T) T {
+	return max(x, y)
 }

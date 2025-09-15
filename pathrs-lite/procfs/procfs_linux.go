cyphar/filepath-securejoin

commit 85aa60b0a0380b934ebb8fe13fcaa1f27b9045ef

pathrs-lite: move new API to subpackage
This code should probably have been in a subpackage from the start
(since it has very little to do with securejoin) but at the time it felt
like a very minimal workaround.

Now that it looks like this is going to be a more permanent solution for
part of the Go ecosystem (and the license for most of this code has been
changed) we should separate it. The name "pathrs-lite" indicates what
the purpose of this subpackage is (i.e., it is a less complete version
of libpathrs).

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

pathrs-lite/procfs/procfs_linux.go
@@ -1,0 +1,128 @@
+// SPDX-License-Identifier: MPL-2.0
+
+//go:build linux
+
+// Copyright (C) 2024-2025 Aleksa Sarai <cyphar@cyphar.com>
+// Copyright (C) 2024-2025 SUSE LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Package procfs provides a safe API for operating on /proc on Linux.
+package procfs
+
+import (
+	"os"
+
+	"github.com/cyphar/filepath-securejoin/pathrs-lite/internal/procfs"
+)
+
+// This package mostly just wraps internal/procfs APIs. This is necessary
+// because we are forced to export some things from internal/procfs in order to
+// avoid some dependency cycle issues, but we don't want users to see or use
+// them.
+
+// ProcThreadSelfCloser is a callback that needs to be called when you are done
+// operating on an [os.File] fetched using [Handle.OpenThreadSelf].
+//
+// [os.File]: https://pkg.go.dev/os#File
+type ProcThreadSelfCloser = procfs.ProcThreadSelfCloser
+
+// Handle is a wrapper around an *os.File handle to "/proc", which can be used
+// to do further procfs-related operations in a safe way.
+type Handle struct {
+	inner *procfs.Handle
+}
+
+// OpenProcRoot tries to open a "safer" handle to "/proc" (i.e., one with the
+// "subset=pid" mount option applied, available from Linux 5.8). Unless you
+// plan to do many operations with [ProcRoot], users should prefer to use this
+// over [OpenUnsafeProcRoot] which is far more dangerous to keep open.
+//
+// If a safe handle cannot be opened, OpenProcRoot will fall back to opening a
+// regular "/proc" handle.
+//
+// Note that using [ProcRoot] will still work with handles returned by this
+// function. If a [ProcRoot] subpath cannot be operated on with a safe "/proc"
+// handle, then [OpenUnsafeProcRoot] will be called internally and a temporary
+// unsafe handle will be used.
+func OpenProcRoot() (*Handle, error) {
+	proc, err := procfs.OpenProcRoot()
+	if err != nil {
+		return nil, err
+	}
+	return &Handle{inner: proc}, nil
+}
+
+// OpenUnsafeProcRoot opens a handle to "/proc" without any overmounts or
+// masked paths. You must be extremely careful to make sure this handle is
+// never leaked to a container and that you program cannot be tricked into
+// writing to arbitrary paths within it.
+//
+// This is not necessary if you just wish to use [ProcRoot], as handles
+// returned by [OpenProcRoot] will fall back to using a *temporary* unsafe
+// handle in that case. You should only really use this if you need to do many
+// operations on [ProcRoot] and the performance overhead of making many procfs
+// handles is an issue, and you should make sure to close the handle as soon as
+// possible to avoid known-fd-number attacks.
+func OpenUnsafeProcRoot() (*Handle, error) {
+	proc, err := procfs.OpenUnsafeProcRoot()
+	if err != nil {
+		return nil, err
+	}
+	return &Handle{inner: proc}, nil
+}
+
+// OpenThreadSelf returns a handle to "/proc/thread-self/<subpath>" (or an
+// equivalent handle on older kernels where "/proc/thread-self" doesn't exist).
+// Once finished with the handle, you must call the returned closer function
+// (runtime.UnlockOSThread). You must not pass the returned *os.File to other
+// Go threads or use the handle after calling the closer.
+func (proc *Handle) OpenThreadSelf(subpath string) (*os.File, ProcThreadSelfCloser, error) {
+	return proc.inner.OpenThreadSelf(subpath)
+}
+
+// OpenSelf returns a handle to /proc/self/<subpath>.
+//
+// Note that in Go programs with non-homogenous threads, this may result in
+// spurious errors. If you are monkeying around with APIs that are
+// thread-specific, you probably want to use [ProcThreadSelf] instead which
+// will guarantee that the handle refers to the same thread as the caller is
+// executing on.
+func (proc *Handle) OpenSelf(subpath string) (*os.File, error) {
+	return proc.inner.OpenSelf(subpath)
+}
+
+// OpenRoot returns a handle to /proc/<subpath>.
+//
+// You should only use this when you need to operate on global procfs files
+// (such as sysctls in /proc/sys). Unlike [OpenThreadSelf], [OpenSelf], and
+// [ProcPid], the procfs handle used internally for this operation will never
+// use subset=pids, which makes it a more juicy target for CVE-2024-21626-style
+// attacks.
+func (proc *Handle) OpenRoot(subpath string) (*os.File, error) {
+	return proc.inner.OpenRoot(subpath)
+}
+
+// OpenPid returns a handle to /proc/$pid/<subpath> (pid can be a pid or tid).
+// This is mainly intended for usage when operating on other processes.
+//
+// You should not use this for the current thread, as special handling is
+// needed for /proc/thread-self (or /proc/self/task/<tid>) when dealing with
+// goroutine scheduling -- use [OpenThreadSelf] instead.
+//
+// To refer to the current thread-group, you should use prefer [OpenSelf] to
+// passing os.Getpid as the pid argument.
+//
+// If you want to operate on the top-level /proc filesystem, you should use
+// [OpenRoot] instead.
+func (proc *Handle) OpenPid(pid int, subpath string) (*os.File, error) {
+	return proc.inner.OpenPid(pid, subpath)
+}
+
+// ProcSelfFdReadlink gets the real path of the given file by looking at
+// readlink(/proc/thread-self/fd/$n).
+func ProcSelfFdReadlink(f *os.File) (string, error) {
+	return procfs.ProcSelfFdReadlink(f)
+}

pathrs-lite/procfs/procfs_linux_test.go
@@ -1,0 +1,155 @@
+// SPDX-License-Identifier: MPL-2.0
+
+//go:build linux
+
+// Copyright (C) 2024-2025 Aleksa Sarai <cyphar@cyphar.com>
+// Copyright (C) 2024-2025 SUSE LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package procfs_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/unix"
+
+	"github.com/cyphar/filepath-securejoin/pathrs-lite/internal/fd"
+	"github.com/cyphar/filepath-securejoin/pathrs-lite/procfs"
+)
+
+// This code is all actually tested in internal/procfs, this is mainly
+// necessary to make sure our one-line wrappers are correct.
+
+func TestOpenProcRoot(t *testing.T) {
+	t.Run("OpenProcRoot", func(t *testing.T) {
+		proc, err := procfs.OpenProcRoot()
+		require.NoError(t, err, "OpenProcRoot")
+		assert.NotNil(t, proc, "procfs *Handle")
+	})
+
+	t.Run("OpenUnsafeProcRoot", func(t *testing.T) {
+		proc, err := procfs.OpenUnsafeProcRoot()
+		require.NoError(t, err, "OpenUnsafeProcRoot")
+		assert.NotNil(t, proc, "procfs *Handle")
+
+		// Make sure the handle actually is !subset=pid.
+		f, err := proc.OpenRoot(".")
+		require.NoError(t, err, "open root .")
+		err = fd.Faccessat(f, "uptime", unix.F_OK, unix.AT_SYMLINK_NOFOLLOW)
+		assert.NoError(t, err, "/proc/uptime should exist") //nolint:testifylint // this is an isolated operation so we can continue despite an error
+	})
+}
+
+type procRootFunc func() (*procfs.Handle, error)
+
+func TestProcRoot(t *testing.T) {
+	for _, test := range []struct {
+		name       string
+		procRootFn procRootFunc
+	}{
+		{"OpenProcRoot", procfs.OpenProcRoot},
+		{"OpenUnsafeProcRoot", procfs.OpenUnsafeProcRoot},
+	} {
+		test := test // copy iterator
+		t.Run(test.name, func(t *testing.T) {
+			proc, err := test.procRootFn()
+			require.NoError(t, err)
+
+			t.Run("OpenThreadSelf", func(t *testing.T) {
+				// Make sure our tid checks below are correct.
+				runtime.LockOSThread()
+				defer runtime.UnlockOSThread()
+
+				stat, closer, err := proc.OpenThreadSelf("stat")
+				require.NoError(t, err, "open /proc/thread-self/stat")
+				if assert.NotNil(t, closer, "closer should be non-nil for /proc/thread-self") {
+					defer closer()
+				}
+				require.NotNil(t, stat, "open /proc/thread-self/stat")
+				defer stat.Close() //nolint:errcheck // test code
+
+				statData, err := os.ReadFile(fmt.Sprintf("/proc/self/fd/%d", stat.Fd()))
+				runtime.KeepAlive(stat)
+				require.NoError(t, err)
+				assert.Regexp(t, fmt.Sprintf("^%d ", unix.Gettid()), string(statData), "/proc/thread-self/stat should have tid prefix")
+
+				// Confirm that this is /proc/$pid/task/$tid, not /proc/$pid.
+				f, closer, err := proc.OpenThreadSelf("task")
+				require.ErrorIs(t, err, os.ErrNotExist, "/proc/thread-self should not have a 'task' dir")
+				if !assert.Nil(t, closer, "returned closer on error") {
+					defer closer()
+				}
+				if !assert.Nil(t, f, "returned *os.File on error") {
+					_ = f.Close()
+				}
+			})
+
+			t.Run("OpenSelf", func(t *testing.T) {
+				stat, err := proc.OpenSelf("stat")
+				require.NoError(t, err, "open /proc/self/stat")
+				require.NotNil(t, stat, "open /proc/self/stat")
+				defer stat.Close() //nolint:errcheck // test code
+
+				statData, err := os.ReadFile(fmt.Sprintf("/proc/self/fd/%d", stat.Fd()))
+				runtime.KeepAlive(stat)
+				require.NoError(t, err)
+				assert.Regexp(t, fmt.Sprintf("^%d ", os.Getpid()), string(statData), "/proc/self/stat should have pid prefix")
+
+				// Confirm that this is /proc/$pid, not /proc/$pid/task/$tid.
+				f, err := proc.OpenSelf("task")
+				require.NoError(t, err, "/proc/self has a 'task' dir")
+				require.NotNil(t, f, "open /proc/self/task")
+				_ = f.Close()
+			})
+
+			t.Run("OpenPid", func(t *testing.T) {
+				stat, err := proc.OpenPid(1, "stat")
+				require.NoError(t, err, "open /proc/1/stat")
+				require.NotNil(t, stat, "open /proc/1/stat")
+				defer stat.Close() //nolint:errcheck // test code
+
+				statData, err := os.ReadFile(fmt.Sprintf("/proc/self/fd/%d", stat.Fd()))
+				runtime.KeepAlive(stat)
+				require.NoError(t, err)
+				assert.Regexp(t, "^1 ", string(statData), "/proc/1/stat should have pid1 prefix")
+
+				// Confirm that this is /proc/$pid, not /proc/$pid/task/$tid.
+				f, err := proc.OpenPid(1, "task")
+				require.NoError(t, err, "/proc/1 has a 'task' dir")
+				require.NotNil(t, f, "open /proc/1/task")
+				_ = f.Close()
+			})
+
+			t.Run("OpenRoot", func(t *testing.T) {
+				uptime, err := proc.OpenRoot("uptime")
+				require.NoError(t, err, "open /proc/uptime")
+				require.NotNil(t, uptime, "open /proc/uptime")
+				defer uptime.Close() //nolint:errcheck // test code
+			})
+		})
+	}
+}
+
+func TestProcSelfFdReadlink(t *testing.T) {
+	root, err := os.Open(".")
+	require.NoError(t, err)
+
+	fullPath, err := procfs.ProcSelfFdReadlink(root)
+	require.NoError(t, err, "ProcSelfFdReadlink")
+
+	cwd, err := os.Getwd()
+	require.NoError(t, err, "getwd")
+	cwd, err = filepath.EvalSymlinks(cwd)
+	require.NoError(t, err, "expand symlinks getwd")
+
+	assert.Equal(t, cwd, fullPath, "ProcSelfFdReadlink('.')")
+}

procfs/procfs_linux.go
@@ -1,128 +1,0 @@
-// SPDX-License-Identifier: MPL-2.0
-
-//go:build linux
-
-// Copyright (C) 2024-2025 Aleksa Sarai <cyphar@cyphar.com>
-// Copyright (C) 2024-2025 SUSE LLC
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-// Package procfs provides a safe API for operating on /proc on Linux.
-package procfs
-
-import (
-	"os"
-
-	"github.com/cyphar/filepath-securejoin/internal/procfs"
-)
-
-// This package mostly just wraps internal/procfs APIs. This is necessary
-// because we are forced to export some things from internal/procfs in order to
-// avoid some dependency cycle issues, but we don't want users to see or use
-// them.
-
-// ProcThreadSelfCloser is a callback that needs to be called when you are done
-// operating on an [os.File] fetched using [Handle.OpenThreadSelf].
-//
-// [os.File]: https://pkg.go.dev/os#File
-type ProcThreadSelfCloser = procfs.ProcThreadSelfCloser
-
-// Handle is a wrapper around an *os.File handle to "/proc", which can be used
-// to do further procfs-related operations in a safe way.
-type Handle struct {
-	inner *procfs.Handle
-}
-
-// OpenProcRoot tries to open a "safer" handle to "/proc" (i.e., one with the
-// "subset=pid" mount option applied, available from Linux 5.8). Unless you
-// plan to do many operations with [ProcRoot], users should prefer to use this
-// over [OpenUnsafeProcRoot] which is far more dangerous to keep open.
-//
-// If a safe handle cannot be opened, OpenProcRoot will fall back to opening a
-// regular "/proc" handle.
-//
-// Note that using [ProcRoot] will still work with handles returned by this
-// function. If a [ProcRoot] subpath cannot be operated on with a safe "/proc"
-// handle, then [OpenUnsafeProcRoot] will be called internally and a temporary
-// unsafe handle will be used.
-func OpenProcRoot() (*Handle, error) {
-	proc, err := procfs.OpenProcRoot()
-	if err != nil {
-		return nil, err
-	}
-	return &Handle{inner: proc}, nil
-}
-
-// OpenUnsafeProcRoot opens a handle to "/proc" without any overmounts or
-// masked paths. You must be extremely careful to make sure this handle is
-// never leaked to a container and that you program cannot be tricked into
-// writing to arbitrary paths within it.
-//
-// This is not necessary if you just wish to use [ProcRoot], as handles
-// returned by [OpenProcRoot] will fall back to using a *temporary* unsafe
-// handle in that case. You should only really use this if you need to do many
-// operations on [ProcRoot] and the performance overhead of making many procfs
-// handles is an issue, and you should make sure to close the handle as soon as
-// possible to avoid known-fd-number attacks.
-func OpenUnsafeProcRoot() (*Handle, error) {
-	proc, err := procfs.OpenUnsafeProcRoot()
-	if err != nil {
-		return nil, err
-	}
-	return &Handle{inner: proc}, nil
-}
-
-// OpenThreadSelf returns a handle to "/proc/thread-self/<subpath>" (or an
-// equivalent handle on older kernels where "/proc/thread-self" doesn't exist).
-// Once finished with the handle, you must call the returned closer function
-// (runtime.UnlockOSThread). You must not pass the returned *os.File to other
-// Go threads or use the handle after calling the closer.
-func (proc *Handle) OpenThreadSelf(subpath string) (*os.File, ProcThreadSelfCloser, error) {
-	return proc.inner.OpenThreadSelf(subpath)
-}
-
-// OpenSelf returns a handle to /proc/self/<subpath>.
-//
-// Note that in Go programs with non-homogenous threads, this may result in
-// spurious errors. If you are monkeying around with APIs that are
-// thread-specific, you probably want to use [ProcThreadSelf] instead which
-// will guarantee that the handle refers to the same thread as the caller is
-// executing on.
-func (proc *Handle) OpenSelf(subpath string) (*os.File, error) {
-	return proc.inner.OpenSelf(subpath)
-}
-
-// OpenRoot returns a handle to /proc/<subpath>.
-//
-// You should only use this when you need to operate on global procfs files
-// (such as sysctls in /proc/sys). Unlike [OpenThreadSelf], [OpenSelf], and
-// [ProcPid], the procfs handle used internally for this operation will never
-// use subset=pids, which makes it a more juicy target for CVE-2024-21626-style
-// attacks.
-func (proc *Handle) OpenRoot(subpath string) (*os.File, error) {
-	return proc.inner.OpenRoot(subpath)
-}
-
-// OpenPid returns a handle to /proc/$pid/<subpath> (pid can be a pid or tid).
-// This is mainly intended for usage when operating on other processes.
-//
-// You should not use this for the current thread, as special handling is
-// needed for /proc/thread-self (or /proc/self/task/<tid>) when dealing with
-// goroutine scheduling -- use [OpenThreadSelf] instead.
-//
-// To refer to the current thread-group, you should use prefer [OpenSelf] to
-// passing os.Getpid as the pid argument.
-//
-// If you want to operate on the top-level /proc filesystem, you should use
-// [OpenRoot] instead.
-func (proc *Handle) OpenPid(pid int, subpath string) (*os.File, error) {
-	return proc.inner.OpenPid(pid, subpath)
-}
-
-// ProcSelfFdReadlink gets the real path of the given file by looking at
-// readlink(/proc/thread-self/fd/$n).
-func ProcSelfFdReadlink(f *os.File) (string, error) {
-	return procfs.ProcSelfFdReadlink(f)
-}

procfs/procfs_linux_test.go
@@ -1,155 +1,0 @@
-// SPDX-License-Identifier: MPL-2.0
-
-//go:build linux
-
-// Copyright (C) 2024-2025 Aleksa Sarai <cyphar@cyphar.com>
-// Copyright (C) 2024-2025 SUSE LLC
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-package procfs_test
-
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"runtime"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"golang.org/x/sys/unix"
-
-	"github.com/cyphar/filepath-securejoin/internal/fd"
-	"github.com/cyphar/filepath-securejoin/procfs"
-)
-
-// This code is all actually tested in internal/procfs, this is mainly
-// necessary to make sure our one-line wrappers are correct.
-
-func TestOpenProcRoot(t *testing.T) {
-	t.Run("OpenProcRoot", func(t *testing.T) {
-		proc, err := procfs.OpenProcRoot()
-		require.NoError(t, err, "OpenProcRoot")
-		assert.NotNil(t, proc, "procfs *Handle")
-	})
-
-	t.Run("OpenUnsafeProcRoot", func(t *testing.T) {
-		proc, err := procfs.OpenUnsafeProcRoot()
-		require.NoError(t, err, "OpenUnsafeProcRoot")
-		assert.NotNil(t, proc, "procfs *Handle")
-
-		// Make sure the handle actually is !subset=pid.
-		f, err := proc.OpenRoot(".")
-		require.NoError(t, err, "open root .")
-		err = fd.Faccessat(f, "uptime", unix.F_OK, unix.AT_SYMLINK_NOFOLLOW)
-		assert.NoError(t, err, "/proc/uptime should exist") //nolint:testifylint // this is an isolated operation so we can continue despite an error
-	})
-}
-
-type procRootFunc func() (*procfs.Handle, error)
-
-func TestProcRoot(t *testing.T) {
-	for _, test := range []struct {
-		name       string
-		procRootFn procRootFunc
-	}{
-		{"OpenProcRoot", procfs.OpenProcRoot},
-		{"OpenUnsafeProcRoot", procfs.OpenUnsafeProcRoot},
-	} {
-		test := test // copy iterator
-		t.Run(test.name, func(t *testing.T) {
-			proc, err := test.procRootFn()
-			require.NoError(t, err)
-
-			t.Run("OpenThreadSelf", func(t *testing.T) {
-				// Make sure our tid checks below are correct.
-				runtime.LockOSThread()
-				defer runtime.UnlockOSThread()
-
-				stat, closer, err := proc.OpenThreadSelf("stat")
-				require.NoError(t, err, "open /proc/thread-self/stat")
-				if assert.NotNil(t, closer, "closer should be non-nil for /proc/thread-self") {
-					defer closer()
-				}
-				require.NotNil(t, stat, "open /proc/thread-self/stat")
-				defer stat.Close() //nolint:errcheck // test code
-
-				statData, err := os.ReadFile(fmt.Sprintf("/proc/self/fd/%d", stat.Fd()))
-				runtime.KeepAlive(stat)
-				require.NoError(t, err)
-				assert.Regexp(t, fmt.Sprintf("^%d ", unix.Gettid()), string(statData), "/proc/thread-self/stat should have tid prefix")
-
-				// Confirm that this is /proc/$pid/task/$tid, not /proc/$pid.
-				f, closer, err := proc.OpenThreadSelf("task")
-				require.ErrorIs(t, err, os.ErrNotExist, "/proc/thread-self should not have a 'task' dir")
-				if !assert.Nil(t, closer, "returned closer on error") {
-					defer closer()
-				}
-				if !assert.Nil(t, f, "returned *os.File on error") {
-					_ = f.Close()
-				}
-			})
-
-			t.Run("OpenSelf", func(t *testing.T) {
-				stat, err := proc.OpenSelf("stat")
-				require.NoError(t, err, "open /proc/self/stat")
-				require.NotNil(t, stat, "open /proc/self/stat")
-				defer stat.Close() //nolint:errcheck // test code
-
-				statData, err := os.ReadFile(fmt.Sprintf("/proc/self/fd/%d", stat.Fd()))
-				runtime.KeepAlive(stat)
-				require.NoError(t, err)
-				assert.Regexp(t, fmt.Sprintf("^%d ", os.Getpid()), string(statData), "/proc/self/stat should have pid prefix")
-
-				// Confirm that this is /proc/$pid, not /proc/$pid/task/$tid.
-				f, err := proc.OpenSelf("task")
-				require.NoError(t, err, "/proc/self has a 'task' dir")
-				require.NotNil(t, f, "open /proc/self/task")
-				_ = f.Close()
-			})
-
-			t.Run("OpenPid", func(t *testing.T) {
-				stat, err := proc.OpenPid(1, "stat")
-				require.NoError(t, err, "open /proc/1/stat")
-				require.NotNil(t, stat, "open /proc/1/stat")
-				defer stat.Close() //nolint:errcheck // test code
-
-				statData, err := os.ReadFile(fmt.Sprintf("/proc/self/fd/%d", stat.Fd()))
-				runtime.KeepAlive(stat)
-				require.NoError(t, err)
-				assert.Regexp(t, "^1 ", string(statData), "/proc/1/stat should have pid1 prefix")
-
-				// Confirm that this is /proc/$pid, not /proc/$pid/task/$tid.
-				f, err := proc.OpenPid(1, "task")
-				require.NoError(t, err, "/proc/1 has a 'task' dir")
-				require.NotNil(t, f, "open /proc/1/task")
-				_ = f.Close()
-			})
-
-			t.Run("OpenRoot", func(t *testing.T) {
-				uptime, err := proc.OpenRoot("uptime")
-				require.NoError(t, err, "open /proc/uptime")
-				require.NotNil(t, uptime, "open /proc/uptime")
-				defer uptime.Close() //nolint:errcheck // test code
-			})
-		})
-	}
-}
-
-func TestProcSelfFdReadlink(t *testing.T) {
-	root, err := os.Open(".")
-	require.NoError(t, err)
-
-	fullPath, err := procfs.ProcSelfFdReadlink(root)
-	require.NoError(t, err, "ProcSelfFdReadlink")
-
-	cwd, err := os.Getwd()
-	require.NoError(t, err, "getwd")
-	cwd, err = filepath.EvalSymlinks(cwd)
-	require.NoError(t, err, "expand symlinks getwd")
-
-	assert.Equal(t, cwd, fullPath, "ProcSelfFdReadlink('.')")
-}

commit 43797e7c4b2be2755ce76a3c6e18abfde2ae2ea4

pathrs-lite: add Close to procfs wrapper
I forgot to include this when adding the wrappers in commit 85aa60b0a038
("pathrs-lite: move new API to subpackage"), which makes correct usage
of (*procfs.Handle) a bit difficult.

Fixes: 85aa60b0a038 ("pathrs-lite: move new API to subpackage")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

pathrs-lite/procfs/procfs_linux.go
@@ -35,6 +35,12 @@ type Handle struct {
 	inner *procfs.Handle
 }
 
+// Close close the resources associated with this Handle. Note that if this
+// Handle was created with [OpenProcRoot], on some kernels the underlying
+// procfs handle is cached and so this Close operation may be a no-op. However,
+// you should always call Close on Handles once you are done with them.
+func (proc *Handle) Close() error { return proc.inner.Close() }
+
 // OpenProcRoot tries to open a "safer" handle to "/proc" (i.e., one with the
 // "subset=pid" mount option applied, available from Linux 5.8). Unless you
 // plan to do many [Handle.OpenRoot] operations, users should prefer to use

pathrs-lite/procfs/procfs_linux_test.go
@@ -34,18 +34,22 @@ func TestOpenProcRoot(t *testing.T) {
 		proc, err := procfs.OpenProcRoot()
 		require.NoError(t, err, "OpenProcRoot")
 		assert.NotNil(t, proc, "procfs *Handle")
+		assert.NoError(t, proc.Close(), "close handle")
 	})
 
 	t.Run("OpenUnsafeProcRoot", func(t *testing.T) {
 		proc, err := procfs.OpenUnsafeProcRoot()
 		require.NoError(t, err, "OpenUnsafeProcRoot")
 		assert.NotNil(t, proc, "procfs *Handle")
+		defer proc.Close() //nolint:errcheck // test code
 
 		// Make sure the handle actually is !subset=pid.
 		f, err := proc.OpenRoot(".")
 		require.NoError(t, err, "open root .")
 		err = fd.Faccessat(f, "uptime", unix.F_OK, unix.AT_SYMLINK_NOFOLLOW)
 		assert.NoError(t, err, "/proc/uptime should exist") //nolint:testifylint // this is an isolated operation so we can continue despite an error
+
+		assert.NoError(t, proc.Close(), "close handle")
 	})
 }
 
@@ -63,6 +67,7 @@ func TestProcRoot(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			proc, err := test.procRootFn()
 			require.NoError(t, err)
+			defer proc.Close() //nolint:errcheck // test code
 
 			t.Run("OpenThreadSelf", func(t *testing.T) {
 				// Make sure our tid checks below are correct.